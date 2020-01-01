51dd77cb399977a7208150f9037cd8fc4a73a9c0
MianMUAmer <m.umairamer@gmail.com>
2020-01-01 05:12:06 +0500

    PBI-29 testing CI/CD Pipeline connectivity

--- Client/fintactic/src/actions/user.js
+++ Client/fintactic/src/actions/user.js
@@ -1,67 +1,68 @@
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAILURE = 'LOGIN_FAILURE';
-export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
-export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+export const LOGOUT_REQUEST = "LOGOUT_REQUEST";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
 
 export function receiveLogin() {
-    return {
-        type: LOGIN_SUCCESS
-    };
+  return {
+    type: LOGIN_SUCCESS,
+  };
 }
 
 function loginError(payload) {
-    return {
-        type: LOGIN_FAILURE,
-        payload,
-    };
+  return {
+    type: LOGIN_FAILURE,
+    payload,
+  };
 }
 
 function requestLogout() {
-    return {
-        type: LOGOUT_REQUEST,
-    };
+  return {
+    type: LOGOUT_REQUEST,
+  };
 }
 
 export function receiveLogout() {
-    return {
-        type: LOGOUT_SUCCESS,
-    };
+  return {
+    type: LOGOUT_SUCCESS,
+  };
 }
 
 // Logs the user out
 export function logoutUser() {
-    return (dispatch) => {
-        dispatch(requestLogout());
-        localStorage.removeItem('authenticated');
-        dispatch(receiveLogout());
-    };
+  return (dispatch) => {
+    dispatch(requestLogout());
+    localStorage.removeItem("authenticated");
+    dispatch(receiveLogout());
+  };
 }
 
 export function loginUser(creds) {
-    return (dispatch) => {
-
-        if (creds.email.length > 0 && creds.password.length > 0) {
-            let opts = {
-                'email': creds.email,
-                'password': creds.password
-            }
-            fetch('/login', {
-                method: 'post',
-                body: JSON.stringify(opts)
-              }).then(r => r.json())
-                .then(response => {
-                if (response.currentuser!='invalid'){
-                    localStorage.setItem('authenticated', true)
-                    dispatch(receiveLogin());
-                    console.log(response.currentuser)
-                  }
-                  else {
-                    console.log("Please type in correct username/password")
-                    dispatch(loginError('Invalid email or password! Please try again.'))
-                  }
-                })
-        } else {
-            dispatch(loginError('Something was wrong. Try again'));
-        }
+  return (dispatch) => {
+    if (creds.email.length > 0 && creds.password.length > 0) {
+      let opts = {
+        email: creds.email,
+        password: creds.password,
+      };
+      fetch("/login", {
+        method: "post",
+        body: JSON.stringify(opts),
+      })
+        .then((r) => r.json())
+        .then((response) => {
+          if (response.currentuser != "invalid") {
+            localStorage.setItem("authenticated", true);
+            dispatch(receiveLogin());
+            console.log(response.currentuser);
+          } else {
+            console.log("Please type in correct username/password");
+            dispatch(
+              loginError("Invalid email or password! Please try again.")
+            );
+          }
+        });
+    } else {
+      dispatch(loginError("Something was wrong. Please Try Again"));
     }
+  };
 }
