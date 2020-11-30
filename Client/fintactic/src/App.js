8596bba3c50573980e62163fbc2620786681005e
MianMUAmer <m.umairamer@gmail.com>
2020-11-30 18:04:56 +0300

    Checking the PR request in github

--- Client/fintactic/src/App.js
+++ Client/fintactic/src/App.js
@@ -18,6 +18,7 @@ function App() {
         <p>
           This will be our Project
         </p>
+        <p>A successful PR request</p>
         <a
           className="App-link"
           href="https://reactjs.org"
