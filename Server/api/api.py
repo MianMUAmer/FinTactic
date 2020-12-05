d92904889345d593f4facbb3fc763e16fbfb8ab9
MianMUAmer <m.umairamer@gmail.com>
2020-12-05 23:18:25 +0300

    simple Registeration api created

--- Server/api/api.py
+++ Server/api/api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, abort, request, jsonify
 import time
 
 app = Flask(__name__)
@@ -10,6 +10,18 @@ def hello():
 @app.route("/time")
 def getTime():
   return {'time' : time.time()}
+
+@app.route('/api/register', methods = ['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400) # missing arguments
+    # --- To check if the user is already in the DB ---
+    # if User.query.filter_by(username = username).first() is not None:
+    #     abort(400) # existing user
+
+    return (jsonify({ 'username': username, 'password': password }), 201)
 
 if __name__ == "__main__":
   app.run()
