5b4ae1597bafed6c8d260263f41afb4ae900e1a2
Beyza Yildirim <beyzayildirim96@hotmail.com>
2020-12-12 21:58:46 +0300

    getting time from flask in react template

---
+++ Client/fintactic/src/components/App.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Switch, Route, Redirect } from 'react-router';
+import { HashRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import {useEffect, useState} from 'react';
+
+/* eslint-disable */
+import ErrorPage from '../pages/error';
+/* eslint-enable */
+
+import '../styles/theme.scss';
+import LayoutComponent from '../components/Layout';
+import Login from '../pages/login';
+import Register from '../pages/register';
+import { logoutUser } from '../actions/user';
+
+const PrivateRoute = ({dispatch, component, ...rest }) => {
+    if (!Login.isAuthenticated(JSON.parse(localStorage.getItem('authenticated')))) {
+        dispatch(logoutUser());
+        return (<Redirect to="/login"/>)
+    } else {
+        return ( // eslint-disable-line
+            <Route {...rest} render={props => (React.createElement(component, props))}/>
+        );
+    }
+};
+
+const CloseButton = ({closeToast}) => <i onClick={closeToast} className="la la-close notifications-close"/>
+
+class App extends React.PureComponent {
+    
+  render() {
+   
+    return (
+        <div>
+            <p>Time Coming from Flask Backend : {this.props.BE_time} </p>
+            <ToastContainer
+                autoClose={5000}
+                hideProgressBar
+                closeButton={<CloseButton/>}
+            />
+            <HashRouter>
+                <Switch>
+                    <Route path="/" exact render={() => <Redirect to="/app/main"/>}/>
+                    <Route path="/app" exact render={() => <Redirect to="/app/main"/>}/>
+                    <PrivateRoute path="/app" dispatch={this.props.dispatch} component={LayoutComponent}/>
+                    <Route path="/register" exact component={Register}/>
+                    <Route path="/login" exact component={Login}/>
+                    <Route path="/error" exact component={ErrorPage}/>
+                    <Route component={ErrorPage}/>
+                    <Redirect from="*" to="/app/main/dashboard"/>
+                </Switch>
+            </HashRouter>
+        </div>
+
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+const FuncComp = () => {
+    const [BE_time, setBE_time] = useState(1);
+  
+    useEffect(() => {
+        fetch('/time').then(res => res.json())
+        .then(data => {
+          setBE_time(data.time);
+        })
+      }, []);
+  
+    return <App BE_time={BE_time}/>
+  }
+  
+export { FuncComp };
+export default connect(mapStateToProps)(App);
---
+++ Client/fintactic/src/pages/correlate/Correlate.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Row, Col } from "reactstrap";
+
+import Widget from "../../components/Widget";
+
+const Correlate = () => (
+  <div>
+    <h1 className="page-title">
+      Correlate
+    </h1>
+    
+  </div>
+);
+
+export default Correlate;
