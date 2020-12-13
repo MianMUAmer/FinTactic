6b9e074ad0a174c1ccbfdc8b0949112099944ee0
Mian Muhammad Umair Amer <34229327+MianMUAmer@users.noreply.github.com>
2020-12-14 01:12:14 +0300

    Kasim's BE and DB config (#4)
    * login/registration (dummy)
    
    * login / registration api is completed
    
    Co-authored-by: kasimvarol <kasim.varol@ug.bilkent.edu.tr>

---
+++ Server/api/flaskblog/models.py
@@ -0,0 +1,23 @@
+from flaskblog import db, login_manager
+from datetime import datetime
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpeg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+    def to_json(self):        
+        return {"username": self.username, "email": self.email, "image":self.image_file}
+
+db.create_all()
+db.session.commit()
---
+++ Server/api/flaskblog/routes.py
@@ -0,0 +1,55 @@
+from flask import render_template, url_for, flash, redirect, request, jsonify
+from flaskblog import app, db, bcrypt
+from flaskblog.models import User
+from flask_login import login_user, current_user, logout_user, login_required
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    username = request.json.get('username')
+    email = request.json.get('email')
+    password = request.json.get('password')
+    errors = {}
+    user = User.query.filter_by(username=username).first()
+    if user:
+        errors['username'] = 'exists'
+    mail = User.query.filter_by(email=email).first()
+    if mail:
+        errors['email'] = 'exists'
+
+    if(len(errors)==0):
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        user = User(username = username, email=email, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        return {'success':'true'}, 200
+    return jsonify(errors), 400
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    email = request.json.get('email')
+    password = request.json.get('password')
+    user = User.query.filter_by(email=email).first()
+    if user and bcrypt.check_password_hash(user.password, password):
+        login_user(user, False)
+        return {'currentuser': current_user.to_json()}, 200
+    return {'currentuser': "invalid"}, 400
+    
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return {"success": 200}
+    
+@app.route("/account")
+@login_required
+def account():
+    
