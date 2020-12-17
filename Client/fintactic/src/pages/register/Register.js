4c1c5e53ae709de8bf8056ebf8e5fa1fdd71e41f
TunjayTaghiyev <34271445+TunjayTaghiyev@users.noreply.github.com>
2020-12-17 23:26:33 +0300

    Minor changes regarding the register (#7)

--- Client/fintactic/src/pages/register/Register.js
+++ Client/fintactic/src/pages/register/Register.js
@@ -87,7 +87,7 @@ class Register extends React.Component {
         return (
             <div className="auth-page">
                 <Container>
-                    <Widget className="widget-auth mx-auto" title={<h3 className="mt-0">Login to your Web App</h3>}>
+                    <Widget className="widget-auth mx-auto" title={<h3 className="mt-0">Register</h3>}>
                         <p className="widget-auth-info">
                             Please fill all fields below.
                         </p>
@@ -142,72 +142,15 @@ class Register extends React.Component {
                                 <Button type="submit" color="danger" className="auth-btn"
                                         size="sm" style={{color: '#fff'}}>{this.props.isFetching ? 'Loading...' : 'Register'}</Button>
                                 <p className="widget-auth-info mt-4">
-                                    Already have the account? Login now!
+                                    Already have the account?
                                 </p>
-                                <Link className="d-block text-center mb-4" to="login">Enter the account</Link>
-                                <div className="social-buttons">
-                                    <Button color="primary" className="social-button">
-                                        <i className="social-icon social-google"/>
-                                        <p className="social-text">GOOGLE</p>
-                                    </Button>
-                                    <Button color="success" className="social-button">
-                                        <i className="social-icon social-microsoft"
-                                           style={{backgroundImage: `url(${microsoft})`}}/>
-                                        <p className="social-text" style={{color: '#fff'}}>MICROSOFT</p>
-                                    </Button>
-                                </div>
+                                <Link className="d-block text-center mb-4" to="login">Login Now!</Link>
                             </div>
                         </form>
                     </Widget>
-                    {/*<Widget className="widget-auth mx-auto" title={<h3 className="mt-0">Create an account</h3>}>*/}
-                        {/*<p className="widget-auth-info">*/}
-                            {/*Please fill all fields below*/}
-                        {/*</p>*/}
-                        {/*<form className="mt" onSubmit={this.doRegister}>*/}
-                            {/*{*/}
-                                {/*this.props.errorMessage && (*/}
-                                    {/*<Alert className="alert-sm" color="danger">*/}
-                                        {/*{this.props.errorMessage}*/}
-                                    {/*</Alert>*/}
-                                {/*)*/}
-                            {/*}*/}
-                            {/*<div className="form-group">*/}
-                                {/*<input className="form-control no-border" value={this.state.email}*/}
-                                       {/*onChange={this.changeEmail} type="text" required name="email"*/}
-                                       {/*placeholder="Email"/>*/}
-                            {/*</div>*/}
-                            {/*<div className="form-group">*/}
-                                {/*<input className="form-control no-border" value={this.state.password}*/}
-                                       {/*onChange={this.changePassword} type="password" required name="password"*/}
-                                       {/*placeholder="Password"/>*/}
-                            {/*</div>*/}
-                            {/*<div className="form-group">*/}
-                                {/*<input className="form-control no-border" value={this.state.confirmPassword}*/}
-                                       {/*onChange={this.changeConfirmPassword} onBlur={this.checkPassword} type="password" required name="confirmPassword"*/}
-                                       {/*placeholder="Confirm"/>*/}
-                            {/*</div>*/}
-                            {/*<Button type="submit" color="inverse" className="auth-btn mb-3" size="sm">{this.props.isFetching ? 'Loading...' : 'Register'}</Button>*/}
-                            {/*<p className="widget-auth-info">or sign up with</p>*/}
-                            {/*<div className="social-buttons">*/}
-                                {/*<Button onClick={this.googleLogin} color="primary" className="social-button mb-2">*/}
-                                    {/*<i className="social-icon social-google"/>*/}
-                                    {/*<p className="social-text">GOOGLE</p>*/}
-                                {/*</Button>*/}
-                                {/*<Button onClick={this.microsoftLogin} color="success" className="social-button">*/}
-                                    {/*<i className="social-icon social-microsoft"*/}
-                                       {/*style={{backgroundImage: `url(${microsoft})`}}/>*/}
-                                    {/*<p className="social-text">MICROSOFT</p>*/}
-                                {/*</Button>*/}
-                            {/*</div>*/}
-                        {/*</form>*/}
-                        {/*<p className="widget-auth-info">*/}
-                            {/*Already have the account? Login now!*/}
-                        {/*</p>*/}
-                        {/*<Link className="d-block text-center" to="login">Enter the account</Link>*/}
-                    {/*</Widget>*/}
                 </Container>
                 <footer className="auth-footer">
-                {new Date().getFullYear()} &copy; Light Blue Template - React Admin Dashboard Template Made by <a href="https://flatlogic.com" rel="noopener noreferrer" target="_blank">Flatlogic LLC</a>.                    
+                {new Date().getFullYear()} &copy; All rights reserved! <a href="" rel="noopener noreferrer" target="_blank">FinTacTic</a>.                    
                 </footer>
             </div>
         );
