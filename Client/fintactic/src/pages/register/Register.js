77a4e94c35f1ffa1d85a2d6ae03fd12f3029fdbd
Tunjay Taghiyev <tunjay.taghiyev@ug.bilkent.edu.tr>
2020-12-19 03:37:28 +0300

    Improvements on Registration (mainly) and Login Page

--- Client/fintactic/src/pages/register/Register.js
+++ Client/fintactic/src/pages/register/Register.js
@@ -5,8 +5,9 @@ import { connect } from 'react-redux';
 import { Container, Alert, Button, FormGroup, InputGroup, InputGroupAddon, InputGroupText, Input, Label } from 'reactstrap';
 import Widget from '../../components/Widget';
 import { registerUser, registerError } from '../../actions/register';
-import microsoft from '../../images/microsoft.png';
 import Login from '../login';
+import Logo from 'C:/Users/user/Desktop/FinTactic/Client/fintactic/src/images/people/a2.jpg';
+
 
 class Register extends React.Component {
     static propTypes = {
@@ -77,7 +78,7 @@ class Register extends React.Component {
     render() {
         const {from} = this.props.location.state || {from: {pathname: '/app'}}; // eslint-disable-line
 
-        // cant access login page while logged in
+        //Cant access login page while logged in
         if (Login.isAuthenticated(JSON.parse(localStorage.getItem('authenticated')))) {
             return (
                 <Redirect to={from}/>
@@ -86,68 +87,74 @@ class Register extends React.Component {
 
         return (
             <div className="auth-page">
-                <Container>
-                    <Widget className="widget-auth mx-auto" title={<h3 className="mt-0">Register</h3>}>
-                        <p className="widget-auth-info">
-                            Please fill all fields below.
-                        </p>
-                        <form onSubmit={this.doRegister}>
-                            {
-                                this.props.errorMessage && (
-                                    <Alert className="alert-sm widget-middle-overflow rounded-0" color="danger">
-                                        {this.props.errorMessage}
-                                    </Alert>
-                                )
-                            }
-                            <FormGroup className="mt">
-                                <Label for="email">Email</Label>
-                                <InputGroup className="input-group-no-border">
-                                    <InputGroupAddon addonType="prepend">
-                                        <InputGroupText>
-                                            <i className="la la-user text-white"/>
-                                        </InputGroupText>
-                                    </InputGroupAddon>
-                                    <Input id="email" className="input-transparent pl-3" value={this.state.email}
-                                           onChange={this.changeEmail} type="email"
-                                           required name="email" placeholder="Email"/>
-                                </InputGroup>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="password">Password</Label>
-                                <InputGroup className="input-group-no-border">
-                                    <InputGroupAddon addonType="prepend">
-                                        <InputGroupText>
-                                            <i className="la la-lock text-white"/>
-                                        </InputGroupText>
-                                    </InputGroupAddon>
-                                    <Input id="password" className="input-transparent pl-3" value={this.state.password}
-                                           onChange={this.changePassword} type="password"
-                                           required name="password" placeholder="Password"/>
-                                </InputGroup>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="confirmPassword">Confirm</Label>
-                                <InputGroup className="input-group-no-border">
-                                    <InputGroupAddon addonType="prepend">
-                                        <InputGroupText>
-                                            <i className="la la-lock text-white"/>
-                                        </InputGroupText>
-                                    </InputGroupAddon>
-                                    <Input id="confirmPassword" className="input-transparent pl-3" value={this.state.confirmPassword}
-                                           onChange={this.changeConfirmPassword} onBlur={this.checkPassword} type="password"
-                                           required name="confirmPassword" placeholder="Confirm"/>
-                                </InputGroup>
-                            </FormGroup>
-                            <div className="bg-widget-transparent auth-widget-footer">
-                                <Button type="submit" color="danger" className="auth-btn"
-                                        size="sm" style={{color: '#fff'}}>{this.props.isFetching ? 'Loading...' : 'Register'}</Button>
-                                <p className="widget-auth-info mt-4">
-                                    Already have the account?
-                                </p>
-                                <Link className="d-block text-center mb-4" to="login">Login Now!</Link>
-                            </div>
-                        </form>
-                    </Widget>
+                
+                <Container id="containerRegistration">
+                    <div className="wrapper">
+                        <div className="wrapperFintactic">
+                        <img src={Logo}></img>
+                        </div>
+                    </div>
+                    <div className="wrapper">
+                        <Widget className="widget-auth mx-auto" title={<h3 className="mt-0">Register</h3>}>
+                            <form onSubmit={this.doRegister}>
+                                {
+                                    this.props.errorMessage && (
+                                        <Alert className="alert-sm widget-middle-overflow rounded-0" color="danger">
+                                            {this.props.errorMessage}
+                                        </Alert>
+                                    )
+                                }
+                                <FormGroup className="mt">
+                                    <Label for="email">Email</Label>
+                                    <InputGroup className="input-group-no-border">
+                                        <InputGroupAddon addonType="prepend">
+                                            <InputGroupText>
+                                                <i className="la la-user text-white"/>
+                                            </InputGroupText>
+                                        </InputGroupAddon>
+                                        <Input id="email" className="input-transparent pl-3" value={this.state.email}
+                                            onChange={this.changeEmail} type="email"
+                                            required name="email" placeholder="Email"/>
+                                    </InputGroup>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="password">Password</Label>
+                                    <InputGroup className="input-group-no-border">
+                                        <InputGroupAddon addonType="prepend">
+                                            <InputGroupText>
+                                                <i className="la la-lock text-white"/>
+                                            </InputGroupText>
+                                        </InputGroupAddon>
+                                        <Input id="password" className="input-transparent pl-3" value={this.state.password}
+                                            onChange={this.changePassword} type="password"
+                                            required name="password" placeholder="Password"/>
+                                    </InputGroup>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="confirmPassword">Confirm</Label>
+                                    <InputGroup className="input-group-no-border">
+                                        <InputGroupAddon addonType="prepend">
+                                            <InputGroupText>
+                                                <i className="la la-lock text-white"/>
+                                            </InputGroupText>
+                                        </InputGroupAddon>
+                                        <Input id="confirmPassword" className="input-transparent pl-3" value={this.state.confirmPassword}
+                                            onChange={this.changeConfirmPassword} onBlur={this.checkPassword} type="password"
+                                            required name="confirmPassword" placeholder="Confirm"/>
+                                    </InputGroup>
+                                </FormGroup>
+                                <div className="bg-widget-transparent auth-widget-footer">
+                                    <Button type="submit" color="danger" className="auth-btn"
+                                            size="sm" style={{color: '#fff'}}>{this.props.isFetching ? 'Loading...' : 'Register'}</Button>
+                                    <p className="widget-auth-info mt-4">
+                                        Already have the account?
+                                    </p>
+                                    <Link className="d-block text-center mb-4" to="login">Login Now!</Link>
+                                </div>
+                            </form>
+                        </Widget>
+
+                    </div>
                 </Container>
                 <footer className="auth-footer">
                 {new Date().getFullYear()} &copy; All rights reserved! <a href="" rel="noopener noreferrer" target="_blank">FinTacTic</a>.                    
