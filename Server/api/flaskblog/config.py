dbba32e2ccb23ff71741a99eabe3466d79a46cc5
kasimvarol <kasim.varol@ug.bilkent.edu.tr>
2021-02-24 17:50:46 +0300

    new database with settings

--- Server/api/flaskblog/config.py
+++ Server/api/flaskblog/config.py
@@ -1,5 +1,5 @@
 CONFIG = {
-   'postgresUrl':'fintactic.cbtwuclmyzf8.eu-central-1.rds.amazonaws.com:5432',
+   'postgresUrl':'fintactic.cdxnigx1rcdj.eu-central-1.rds.amazonaws.com:5432',
    'postgresUser':'postgres',
    'postgresPass':'Fintactic++',
    'postgresDb':'fintactic_db',
