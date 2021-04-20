6b9f309d841dc2ff1c5d3acc23d69a5d6c5f3d63
kasimvarol <kasim.varol@ug.bilkent.edu.tr>
2021-04-20 22:56:39 +0300

    add report functionality

--- Server/api/flaskblog/models.py
+++ Server/api/flaskblog/models.py
@@ -11,12 +11,26 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpeg')
     password = db.Column(db.String(60), nullable=False)
+    reports = db.relationship('Report', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.email}', '{self.image_file}')"
 
     def to_json(self):        
         return {"email": self.email, "image":self.image_file}
+
+class Report(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300))
+    date_uploaded = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    data = db.Column(db.LargeBinary)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    def get_meta(self):
+        return {"id":self.id, "title": self.title, "date": self.date_uploaded.strftime("%d.%m.%Y")}
+    def get_data(self):
+        return self.data
+    def get_date(self):
+        return self.date_uploaded.strftime("%d.%m.%Y")
 
 class AAPL(db.Model):
     date = db.Column(db.String(20), primary_key=True)
@@ -170,3 +184,5 @@ class SI(db.Model):
 
     def to_json(self):
         return {"1. open": self.open, "2. high":self.high, "3. low":self.low, "4. close":self.close, "5. adjclose":self.adjclose, "6. volume":self.volume, "7. dividend amout": "0.0000", "8. split coefficient": "1.0" }
+
+
