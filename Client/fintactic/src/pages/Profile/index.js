5dae83545fe5bc31f7926f1afa1f4416bcce70fb
MianMUAmer <m.umairamer@gmail.com>
2021-05-07 01:15:06 +0300

    Profile page bug fixed

--- Client/fintactic/src/pages/Profile/index.js
+++ Client/fintactic/src/pages/Profile/index.js
@@ -45,7 +45,6 @@ class Profile extends React.Component {
         return response.json();
       })
       .then((data) => {
-        console.log(data);
         this.setState({
           name: data.name,
           email: data.email,
@@ -83,9 +82,9 @@ class Profile extends React.Component {
         return response.blob();
       })
       .then((data) => {
-        console.log(data);
         this.setState({
           picName: URL.createObjectURL(data),
+          LpicName: data,
         });
       });
   };
@@ -190,12 +189,12 @@ class Profile extends React.Component {
     formData.append("instagram", this.state.Linstagram);
     formData.append("facebook", this.state.Lfacebook);
 
-    fetch("/updateUser", {
-      method: "post",
-      body: formData,
-    }).then((response) => {
-      console.log(this.state.LpicName);
-      this.setState({
+    // fetch("/updateUser", {
+    //   method: "post",
+    //   body: formData,
+    // }).then((response) => {
+    this.setState(
+      {
         name: this.state.Lname,
         email: this.state.Lemail,
         phone: this.state.Lphone,
@@ -207,8 +206,14 @@ class Profile extends React.Component {
         instagram: this.state.Linstagram,
         facebook: this.state.Lfacebook,
         showModal: false,
-      });
-    });
+      },
+      () => {
+        fetch("/updateUser", {
+          method: "post",
+          body: formData,
+        });
+      }
+    );
   };
 
   render() {
@@ -248,7 +253,6 @@ class Profile extends React.Component {
                   <div className="d-flex flex-column align-items-center text-center">
                     <img
                       src={picName}
-                      // src="https://bootdey.com/img/Content/avatar/avatar7.png"
                       alt="Admin"
                       className="rounded-circle"
                       width={150}
